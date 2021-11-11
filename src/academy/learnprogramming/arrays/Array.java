3596797c592f4675c44cbe1229e528161625c919
munir sanzar <msanzar87@gmail.com>
2021-11-11 01:51:52 -0500

    find array index complete

--- src/academy/learnprogramming/arrays/Array.java
+++ src/academy/learnprogramming/arrays/Array.java
@@ -12,13 +12,11 @@ public class Array {
         nums[5] = 12;
         nums[6] = 10;
 
-        int index = -1;
-
+        int index = 0;
         for (int i = 0; i < nums.length; i++) {
-        if (nums[i] == 12){
-            index = i;
-            break;
-
+            if (nums[i] == -1){
+                index = i;
+                break;
             }
         }
         System.out.println("Index = "+ index);
