797105447eba5e9f00ba73f3db6fc3d1797ee664
munir sanzar <msanzar87@gmail.com>
2021-11-11 01:45:19 -0500

    make a program to find array index

--- src/academy/learnprogramming/arrays/Array.java
+++ src/academy/learnprogramming/arrays/Array.java
@@ -7,18 +7,23 @@ public class Array {
         nums[0] = 1;
         nums[1] = 2;
         nums[2] = 4;
-        nums[3] = 0;
+        nums[3] = 7;
         nums[4] = -1;
         nums[5] = 12;
         nums[6] = 10;
 
+        int index = -1;
+
         for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);   
+
+            }
         }
+
+
 
         int[] array = {1,3,4,6,7,8,3};
         for (int j = 0; j < array.length; j++) {
-            System.out.println("Index " + array[j]);
+            System.out.println("Index " + array[j] * 2 );
 
         }
 
