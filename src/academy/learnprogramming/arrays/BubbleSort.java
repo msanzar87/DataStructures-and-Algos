77b0f1a1a73e9e53e2a24e07e31b2337ea07d107
munir sanzar <msanzar87@gmail.com>
2021-11-13 02:03:35 -0500

    bubblesort3 challenge

--- src/academy/learnprogramming/arrays/BubbleSort.java
+++ src/academy/learnprogramming/arrays/BubbleSort.java
@@ -4,11 +4,11 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
 
-        for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
-            for (int i = 0; i < lastUnsortedIndex; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr, i, i + 1);
-                }
+        for (int lastUnsorted = arr.length - 1; lastUnsorted > 0; lastUnsorted--) {
+            for (int i = 0; i < lastUnsorted; i++) {
+if (arr[i] > arr[i+1]){
+    swap(arr, i, i+1);
+}
             }
         }
         for (int i = 0; i < arr.length; i++) {
@@ -29,6 +29,14 @@ public class BubbleSort {
 //            System.out.println(arr[i]);
 //        }
     
+    }
+    public static void swap(int[] array, int i, int j){
+        if (i==j){
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
