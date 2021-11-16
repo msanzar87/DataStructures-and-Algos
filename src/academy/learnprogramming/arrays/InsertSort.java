999de828d5d8d087735afe7b88a80fbff36094b9
munir sanzar <msanzar87@gmail.com>
2021-11-16 18:16:41 -0500

    InsertSort1 challenges

--- src/academy/learnprogramming/arrays/InsertSort.java
+++ src/academy/learnprogramming/arrays/InsertSort.java
@@ -3,17 +3,27 @@ package academy.learnprogramming.arrays;
 public class InsertSort {
     public static void main(String[] args) {
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
-
-        int n = arr.length;
+      int n = arr.length;
         for (int j = 1; j < n; j++) {
             int key = arr[j];
             int i = j-1;
             while (i > -1 && arr[i] > key){
-                arr[i+1] = arr[i];
-                i--;
+                arr[i+1] = arr[i]; i--;
             }
-            arr[i + 1] = key;
+            arr[i+1] = key;
         }
+
+
+//        int n = arr.length;
+//        for (int j = 1; j < n; j++) {
+//            int key = arr[j];
+//            int i = j-1;
+//            while (i > -1 && arr[i] > key){
+//                arr[i+1] = arr[i];
+//                i--;
+//            }
+//            arr[i + 1] = key;
+//        }
 //        for (int unsort = 1; unsort < arr.length; unsort++) {
 //            int newElement = arr[unsort];
 //            int i;
@@ -23,7 +33,7 @@ public class InsertSort {
 //            arr[unsort] = i;
 //        }
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
