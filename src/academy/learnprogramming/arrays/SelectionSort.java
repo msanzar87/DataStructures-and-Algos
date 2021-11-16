2c5348d8fdf71356f74d47f9c6b56d7ef7d673eb
munir sanzar <msanzar87@gmail.com>
2021-11-15 20:57:28 -0500

    Selectionsort challenges

--- src/academy/learnprogramming/arrays/SelectionSort.java
+++ src/academy/learnprogramming/arrays/SelectionSort.java
@@ -1,13 +1,38 @@
 package academy.learnprogramming.arrays;
 
-import java.util.Arrays;
-
 public class SelectionSort {
+
     public static void main(String[] args) {
         int[] arr = {20, 35, -15, 7, 55, 1, -22};
-        Arrays.sort(arr);
+
+//        for (int unsorted = arr.length - 1; unsorted > 0; unsorted--) {
+//            int largest = 0;
+//            for (int i = 1; i <= unsorted; i++) {
+//                if (arr[i] > arr[largest]) {
+//                    largest = i;
+//                }
+//
+//            }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[index]){
+                    index = j;
+            }
+        }swap(arr, i,index);
+
+        }
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-}
+
+        public static void swap ( int[] array, int i, int j){
+            if (i == j) {
+                return;
+            }
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
