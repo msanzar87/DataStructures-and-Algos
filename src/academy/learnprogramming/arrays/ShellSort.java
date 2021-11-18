c65fa9b36b57a33db1306aa60af740aea4832634
munir sanzar <msanzar87@gmail.com>
2021-11-18 02:34:29 -0500

    Factorial challenges

--- src/academy/learnprogramming/arrays/ShellSort.java
+++ src/academy/learnprogramming/arrays/ShellSort.java
@@ -5,20 +5,20 @@ public class ShellSort {
 
     public static void main(String[] args) {
         int[] nums = {20, 35, -15, 7, 55, 1, -22};
+        //initialize gap and then reduce it
         for (int gap = nums.length / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < nums.length; i++) {
+                //assign new field
                 int newElement = nums[i];
                 int j = i;
 
-                while (j >= gap && nums[j - gap] > newElement){
-                    nums[j] = nums[j-gap];
+                while (j >= gap && nums[j - gap] > newElement) {
+                    nums[j] = nums[j - gap];
                     j -= gap;
                 }
                 nums[j] = newElement;
             }
         }
-
-
 
 
         for (int i = 0; i < nums.length; i++) {
