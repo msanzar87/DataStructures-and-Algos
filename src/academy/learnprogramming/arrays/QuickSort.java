edb9dfff5d0698f048f3c094027d78c63019aaa3
munir sanzar <msanzar87@gmail.com>
2021-11-30 23:06:47 -0500

    Mergesort

---
+++ src/academy/learnprogramming/arrays/QuickSort.java
@@ -0,0 +1,34 @@
+package academy.learnprogramming.arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] nums = {20,35,-15,7,55,1,-22};
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+    public static void mergeSort(int[] input, int start, int end){
+        if (end - start < 2){
+            return;
+        }
+        int mid = start + end / 2;
+        mergeSort(input,start,mid);
+        mergeSort(input,mid,end);
+        merge(input,start,mid,end);
+    }
+    public static void merge(int[] input, int start, int mid , int end){
+
+        if (input(mid - 1) <= input(mid)){
+            return;
+        }
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end){
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+    }
+}
